05b660e398fb0ed20ebe7cf41522926281fb56f2
vatsal-11 <vatsalsheth34@gmail.com>
2021-03-26 14:48:11 +0530

    123

---
+++ cook/lmenu.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+ 
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/lstyle.css">
+     <title>Cook</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img src="images/khaana.png" class="logohere" alt="logo">
+        <a class="navbar-brand" href="lindex.php">KHAANA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item ">
+            <a class="nav-link" href="lindex.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="lcookaboutus.php">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="lcontactcook.php">Contact Us</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="lmenu.php">menu</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="lorder.php">order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="lpackage.php">Our Package Policy</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+         
+          <li class="nav-item" data-target="#login" data-toggle="modal"><a href="#" class="nav-link">Log out</output></a></li>
+               
+            </ul>
+      </div>
+    </nav>
+    
+    <h1> i m menu </h1>
+</body>
+</html>
