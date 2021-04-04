49de37f2539b44f8d2b3d02a307eaaeb133b26a4
akhilshah26 <akhilshah192000@gmail.com>
2021-04-04 17:48:14 +0530

    ok

--- user/contact.php
+++ user/contact.php
@@ -1,61 +1,60 @@
 
-<html>
-<head>
-	<title>user</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-				<!-- navigation bar -->
-<nav class="navbar navbar-expand-lg navbar-light ">
-<img src="images/khaana.png" class="logohere" alt="logo">
-  <a class="navbar-brand" href="index.php">Khaana</a>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>User</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="images/khaana.png" class="logohere" alt="logo">
+  <a class="navbar-brand" href="index.php">KHAANA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aboutus.php">About Us</a>
+      </li>
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="contact.php">Contact Us<span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item ">
-        <a class="nav-link" href="aboutus.php">About us </a>
-      </li>
-      <li class="nav-item ">
-        <a class="nav-link" href="contact.php">Contact us </a>
-      </li>
-            <li class="nav-item">
+      <li class="nav-item">
         <a class="nav-link" href="#">Menu</a>
       </li>
     </ul>
-      <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav ml-auto">
 	  <li class="nav-item text-white">
 	  	<a class="nav-link" href="#">
-		 <?php
-		 	if(isset($_SESSION['cemail'])){
-			 echo "Welcome," . $_SESSION['cemail'];
-			 }
-			 else{
-				 echo "Welcome Guest";
-			 }
-		 ?> 
+		 Welcome Guest 
 		 </a>
 		</li>
-	  <?php 
-		
-		if(isset($_SESSION['cemail'])){
-			echo "<li class='nav-item'><a href='logout.php' class='nav-link'>Log Out</a></li>";
-		}
-		else{
-			echo "<li class='nav-item' data-target='#login' data-toggle='modal'><a href='#' class='nav-link'>Log In</a></li>";
-			
-		}
-	?>
-      	<li class="nav-item " data-target="#signin" data-toggle="modal"><a href="#" class="nav-link">Sign Up</a></li>      </ul>
+	  <li class="nav-item" data-target="#login" data-toggle="modal"><a href="#" class="nav-link">Log In</a></li>
+      <li class="nav-item " data-target="#signin" data-toggle="modal"><a href="#" class="nav-link">Sign Up</a></li>      
+        </ul>
   </div>
 </nav>
+<!--END NAVBAR-->
 <!--SIGNUP MODAL-->
 <div class="container">
 	<div class="modal" id="signin">
@@ -88,6 +87,25 @@
 						<div class="form-group">
 							<label for="inputadd">Address</label>
 							<input type="text" class="form-control" id="inputadd" name="c_add" placeholder="Enter Address" required>
+						</div>
+                        <div class="form-group">
+							<label for="inputadd">Gender</label>
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" name="c_gen" id="flexRadioDefault1" value="Male">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                            Male
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="c_gen" id="flexRadioDefault1" value="Female">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                            Female
+                            </label>
+                        </div>
+                        </div>
+						<div class="form-group">
+							<label for="inputadd">Expertise</label>
+							<input type="text" class="form-control" id="inputadd" name="c_speciality" placeholder="Enter Your Expertise" required>
 						</div>
 						<div class="form-group">
 							<label for="inputpass">Password</label>
@@ -160,85 +178,68 @@
 		</div>
 	</div>
 	</div>
-<!-- Carousel -->
-<div class="container-fluid slider">
+	<!-- logo and form -->
+	<div class="container cook"  >
+  
+	<div class="row">
+		<div class="col-sm-6 col-lg-6" >
+		<img class="img-fluid" src="images/KHHANA.jpg" width="auto" height="auto">
+		</div>
+		<div class="col-sm-6 col-lg-6"  id="grad1" >
+		<h2>CONTACT US</h2>
+		<div class="form-group">
+			<label for="usr">Name:</label>
+			<input type="text" class="form-control" id="usr" name="name" placeholder="enter your name">
+		</div>
+		<div class="form-group">
+			<label for="pwd">Email:</label>
+			<input type="email" class="form-control" id="email" name="email" placeholder="enter your email id">
+		</div>
+		<div class="form-group">
+			<label for="msg">Message</label>
+			<input type="text" class="form-control" id="msg" name="msg" placeholder="message">
+		</div>
+			<div class="row" style="margin-left: 40px;">
+				<div class="col-sm-6" >
+					<button type="button" class="btn btn-primary active">submit</button>
+				</div>
+				<div class="col-sm-6">
+				<button type="button" class="btn btn-primary active">cancel</button>
+				</div>
+			</div>
+		</div>
+	</div>
 
-	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="">
-	  <ol class="carousel-indicators">
-	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-	  </ol>
-	  <div class="carousel-inner">
-	    <div class="carousel-item active" style="margin-top: 30px;">
-	      <img class="d-block w-100" src="images/thali3.jpg" alt="First slide" style="height: 90vh;">
-	    </div>
-	    <div class="carousel-item" style="margin-top: 30px;">
-	      <img class="d-block w-100" src="images/thali.jpg" alt="Second slide" style="height: 90vh;">
-	    </div>
-	    <div class="carousel-item" style="margin-top: 30px;">
-	      <img class="d-block w-100" src="images/thali2.jpg" alt="Third slide" style="height: 90vh;">
-	    </div>
+</div>
+	<!-- logo and form end-->
+<div class="container">
+	<div class="row rowco">
+		<div class="col-sm-4 address"> Address 
+		<ul class="list-group list-group-flush">
+		  <li class="list-group-item address1">rcti.near gujrat high court <br> ahemdabad  </li>
+		
+		</ul>
+		</div>
+		<div class="col-sm-4 phn"> Phone no.
+		<ul class="list-group list-group-flush">
+		  <li class="list-group-item phn1"> 8155443322</li>
+		  
+		</ul>
+		</div>
+		<div class="col-sm-4 email">Email id
+		<ul class="list-group list-group-flush">
+		  <li class="list-group-item email1">tsf@gmail.com </li>
+		  
+		</ul>
+		</div>
+
 	  </div>
-	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-	    <span class="sr-only">Previous</span>
-	  </a>
-	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-	    <span class="sr-only">Next</span>
-	  </a>
-	</div>
-</div>
-<!--Services-->
-<section class="container text-center service">
-	<h1>Packages</h1>
-
-	<div class="row rowsetting">
-		<div class="col-mg-4 col-md-4 col-sm-4 col-1o d-block m-auto colsetting">
-			<div class="imgsetting d-block m-auto bg-warning">
-      <i class="fa fa-calendar fa-3x text-white" aria-hidden="true"></i>
-			</div>
-			<h2>1 WEEK</h2>
-			<p>BOOK A TIFFIN FOR  1 WEEK AND ENJOY YOUR DAILY TIFFIN PROVIDED BY OUR COOKS. COST MAY VARRY BY COOKS</p>
-		</div>
-		<div class="col-mg-4 col-md-4 col-sm-4 col-1o d-block m-auto colsetting">
-			<div class="imgsetting d-block m-auto bg-warning">
-			<i class="fa fa-calendar fa-3x text-white" aria-hidden="true"></i>
-			</div>
-			<h2>1 MONTH</h2>
-			<p>BOOK A TIFFIN FOR  1 WEEK AND ENJOY YOUR DAILY TIFFIN PROVIDED BY OUR COOKS. COST MAY VARRY BY COOKS</p>
-		</div>
-		<div class="col-mg-4 col-md-4 col-sm-4 col-1o d-block m-auto colsetting">
-			<div class="imgsetting d-block m-auto bg-warning">
-      <i class="fa fa-calendar fa-3x text-white" aria-hidden="true"></i>
-			</div>
-			<h2>3 MONTH</h2>
-			<p>BOOK A TIFFIN FOR  1 WEEK AND ENJOY YOUR DAILY TIFFIN PROVIDED BY OUR COOKS. COST MAY VARRY BY COOKS</p>
-		</div>
-	</div>
-</section>
-<!--team-->
-<div class="teams text-center">
-	<div class="container text-center teamcont">
-		<h3 class="text-white">Menu</h3>
-		<h4 class="text-white">BEST PLACE TO GET TASTE OF HAME MADE FOOD</h4>
-	</div>
-	<div class="container">
-		<div class="row text-center"></div>
-	</div>						
-</div>
-<!--Footer-->
-<div class="container-fluid footer text-center">
-	<h4>&copy; Copyright section khaana.com</h4>
-</div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	  </div>
+	 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-</body>
+  </body>
 </html>
-<!--php code to signin a customer-->
-
-
-
