f006b4c790a4e16ae0a99ffbeee4e4e5cfac4348
vatsal-11 <vatsalsheth34@gmail.com>
2021-04-04 16:18:03 +0530

    123

--- user/index.php
+++ user/index.php
@@ -10,23 +10,41 @@
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="images/khaana.png" class="logohere" alt="logo">
+  <a class="navbar-brand" href="index.php">KHAANA</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </div>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cookaboutus.php">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactcook.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="package.php">Our Package Policy</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+	  <li class="nav-item text-white">
+	  	<a class="nav-link" href="#">
+		 Welcome Guest 
+		 </a>
+		</li>
+	  <li class="nav-item" data-target="#login" data-toggle="modal"><a href="#" class="nav-link">Log In</a></li>
+      <li class="nav-item " data-target="#signin" data-toggle="modal"><a href="#" class="nav-link">Sign Up</a></li>      
+        </ul>
   </div>
 </nav>
 
-<br><br><br><br>
+
 
 <div class="container" >
    
@@ -65,7 +83,7 @@
   </div>
 </div>
 
-<br><br>
+.
 <div class="container text-center ">
 		<h3>Packages</h3><br>
 	   </div>
