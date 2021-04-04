15aa1862a906f5b131dbcdaf2e38ecd0fd54fc7c
akhilshah26 <akhilshah192000@gmail.com>
2021-04-04 15:31:52 +0530

    ok

---
+++ user/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+    </div>
+  </div>
+</nav>
+
+<br><br><br><br>
+
+<div class="container" >
+   
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="images/1.jpg"  >
+      </div>
+
+      <div class="item">
+        <img src="images/1000.PNG" >
+      </div>
+    
+      <div class="item">
+        <img src="images/hehehehe.PNG" >
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<br><br>
+<div class="container text-center ">
+		<h3>Packages</h3><br>
+	   </div>
+      
+     <div class="container ">
+	<div class="row rowco">
+		<div class="col-sm-4 address"><h4><b>1 WEEK </b></h4>
+		<ul class="list-group list-group-flush">
+		  <li class="list-group-item address1">abhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsd</li>
+		
+		</ul>
+		</div>
+		<div class="col-sm-4 phn"> <h4><b>1 MONTH </b></h4>
+		<ul class="list-group list-group-flush">
+		  <li class="list-group-item phn1"> abhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsd</li>
+		  
+		</ul>
+		</div>
+		<div class="col-sm-4 email"><h4><b>3 MONTH </b></h4>
+		<ul class="list-group list-group-flush">
+		  <li class="list-group-item email1">abhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsd</li>
+		  
+		</ul>
+		</div>
+
+	  </div>
+	  </div>
+
+    
+    <br><br>
+<div class="container text-center ">
+		<h3>MENU</h3><br>
+	   </div> 
+
+     <div class="container">
+<div class="row ">
+<div class="col-md-4 col-sm-4 py-3 py-sm-0">
+
+	<div class="card" style="width:20rem;">
+		<img src="images/1.jpg" class="card-img-top img-fluid"  alt="...">
+		  <ul class="list-group list-group-flush">
+		  <li class="list-group-item">Price :- 20 rps</li>
+		  	</ul>
+  </div>
+
+</div>
+
+
+<div class="col-md-4 col-sm-4 py-3 py-sm-0">
+
+	<div class="card" style="width:20rem;">
+		<img src="images/1.jpg" class="card-img-top img-fluid"  alt="...">
+		  <ul class="list-group list-group-flush">
+		  <li class="list-group-item">Price :- 20 rps</li>
+		  	</ul>
+  </div>
+
+</div>
+
+<div class="col-md-4 col-sm-4 py-3 py-sm-0">
+
+	<div class="card" style="width:20rem;">
+		<img src="images/1.jpg" class="card-img-top img-fluid"  alt="...">
+		  <ul class="list-group list-group-flush">
+		  <li class="list-group-item">Price :- 20 rps</li>
+		  	</ul>
+  </div>
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
