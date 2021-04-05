f3ea848866fee3f608392e15836b86d044a202f7
vatsal-11 <vatsalsheth34@gmail.com>
2021-04-05 11:47:52 +0530

    123

--- user/index.php
+++ user/index.php
@@ -1,4 +1,3 @@
-
 <html>
 <head>
 	<title>user</title>
@@ -7,91 +6,6 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-<<<<<<< HEAD
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <img src="images/khaana.png" class="logohere" alt="logo">
-  <a class="navbar-brand" href="index.php">KHAANA</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="cookaboutus.php">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contactcook.php">Contact Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="package.php">Our Package Policy</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav ml-auto">
-	  <li class="nav-item text-white">
-	  	<a class="nav-link" href="#">
-		 Welcome Guest 
-		 </a>
-		</li>
-	  <li class="nav-item" data-target="#login" data-toggle="modal"><a href="#" class="nav-link">Log In</a></li>
-      <li class="nav-item " data-target="#signin" data-toggle="modal"><a href="#" class="nav-link">Sign Up</a></li>      
-        </ul>
-  </div>
-</nav>
-
-
-
-<div class="container" >
-   
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="images/1.jpg"  >
-      </div>
-
-      <div class="item">
-        <img src="images/1000.PNG" >
-      </div>
-    
-      <div class="item">
-        <img src="images/hehehehe.PNG" >
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
-.
-<div class="container text-center ">
-		<h3>Packages</h3><br>
-	   </div>
-      
-     <div class="container ">
-	<div class="row rowco">
-		<div class="col-sm-4 address"><h4><b>1 WEEK </b></h4>
-		<ul class="list-group list-group-flush">
-		  <li class="list-group-item address1">abhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsdabhi k liya kuch bhi likho na sdfskdsfksdfdskdkkddsd</li>
-=======
 				<!-- navigation bar -->
 <nav class="navbar navbar-expand-lg navbar-light ">
 <img src="images/khaana.png" class="logohere" alt="logo">
@@ -129,7 +43,6 @@
 		 </a>
 		</li>
 	  <?php 
->>>>>>> 475ee7d643a3b03b047cd9c75abedc10ef1a7948
 		
 		if(isset($_SESSION['cemail'])){
 			echo "<li class='nav-item'><a href='logout.php' class='nav-link'>Log Out</a></li>";
@@ -325,6 +238,3 @@
 </body>
 </html>
 <!--php code to signin a customer-->
-
-
-
