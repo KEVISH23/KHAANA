4900e1b4f194966b826d7e519685d4293dbfdaec
akhilshah26 <akhilshah192000@gmail.com>
2021-04-11 16:11:53 +0530

    ok

--- cook/package.php
+++ cook/package.php
@@ -6,7 +6,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+	<link href='https://fonts.googleapis.com/css?family=Berkshire Swash' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Cinzel Decorative' rel='stylesheet'>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -17,7 +18,7 @@
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <img src="images/khaana.png" class="logohere" alt="logo">
-  <a class="navbar-brand" href="index.php">KHAANA</a>
+  <a class="navbar-brand" style="font-family: 'Berkshire Swash';font-size: 22px;" href="index.php">Khaana</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -39,7 +40,7 @@
     </ul>
     <ul class="navbar-nav ml-auto">
 	  <li class="nav-item text-white">
-	  	<a class="nav-link" href="#">
+	  	<a class="nav-link" href="#" style="font-family: 'Cinzel Decorative';font-size: 22px;">
 		 Welcome Guest 
 		 </a>
 		</li>
