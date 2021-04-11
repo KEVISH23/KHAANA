36b6c343a130596d31009e603d28772e6679fa4d
vatsal-11 <vatsalsheth34@gmail.com>
2021-04-11 14:22:54 +0530

    123\

--- Admin/AdminIndex.php
+++ Admin/AdminIndex.php
@@ -5,11 +5,8 @@ include("functions.php");
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD:Admin/AdminIndex.php
     <title>Dashboard</title>
-=======
     <title>admin </title>
->>>>>>> e00d5d3c0b254fd3258de892278b034c0c980f71:Admin/AdminIndex.html
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
