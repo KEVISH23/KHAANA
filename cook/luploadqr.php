f7f6b5a2a156d2fbdca7a2409c6a2a402ed49730
KEVISH23 <kevishthakkar001@gmail.com>
2021-04-24 17:51:00 +0530

    jai Hind

---
+++ cook/luploadqr.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+if (!isset($_SESSION['uname'])) {
+
+	echo "<script>window.open('index.php','_self')</script>";
+}
+include ("includes.php");
+include ("functions.php");
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href='https://fonts.googleapis.com/css?family=Berkshire Swash' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Cinzel Decorative' rel='stylesheet'>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/lstyle.css">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"> 
+
+    <title>Menu</title>
+  </head>
+  
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="images/khaana.png" class="logohere" alt="logo">
+  <a class="navbar-brand" style="font-family: 'Berkshire Swash';font-size: 22px;" href="lindex.php">Khaana</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="lindex.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="lcookaboutus.php">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="lcontactcook.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="lmenu.php">Menu</a>
+      </li>
+	  
+      <li class="nav-item">
+        <a class="nav-link" href="lorder.php">Order</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="lpackage.php">Our Package Policy</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+		<li class="nav-item" ><a href="#" class="nav-link" style="font-family: 'Cinzel Decorative';font-size: 22px;">
+			<?php
+				echo "Welcome ". $_SESSION['uname'];
+			?>
+		</output></a></li>	
+	  <li class="nav-item" ><a href="logout.php" class="nav-link">Log out</output></a></li>
+           
+        </ul>
+  </div>
+</nav>
+<!--END NAVBAR-->
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb bg-dark mt-3">
+    <li class="breadcrumb-item active">Upload QR</li>
+    <li class="breadcrumb-item"><a href="updateqr.php" style="text-decoration:none;" class="text-white">Update QR</a></li>
+  </ol>
+</nav>
+<p class="display-4 text-center text-white">Upload Your QR Code</p>
+<div class="container mt-4 text-center bg-">
+<form method="post" enctype="multipart/form-data">
+<div class="form-group">
+				<label for="inputimg" class="text-white">Upload QR Code</label>
+				<input type="file" class="form-control" id="inputimg" name="c_img" required>
+	</div>
+ 
+  
+  <button type="submit" name="upload" class="btn btn-primary">Upload</button>
+</form>
+</div>
+
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   
+ </body>
+ </html>
+ <?php
+if (isset($_POST['upload'])) {
+  # code...
+  global $con;
+  $cid = $_SESSION['uid'];
+  $cimage = $_FILES['c_img']['name'];
+	$cimage_tmp = $_FILES['c_img']['tmp_name'];
+  $qry = "select * from qr_code where cook_id = $cid";
+  $res = mysqli_query($con,$qry);
+  if ($res) {
+    $rc = mysqli_num_rows($res);
+      if ($rc == 0) {
+        $q = "insert into qr_code(cook_id,qrcode) values ($cid,'$cimage')";
+  $r = mysqli_query($con,$q);
+  if ($r) {
+    move_uploaded_file($cimage_tmp,"cookqrimages/$cimage");
+    # code...
+   echo "<div class='alert alert-success alert-dismissible fade show fixed-top' role='alert'>
+							<strong>Yeah</strong> Your QR Code Uploaded Successfully..
+							<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+							<span aria-hidden='true'>&times;</span>
+							</button>
+							</div>";
+  }
+  else {
+   echo "<div class='alert alert-danger alert-dismissible fade show fixed-top' role='alert'>
+							<strong>Oops!</strong>Something Went Wrong Please try again later...
+							<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+							<span aria-hidden='true'>&times;</span>
+							</button>
+							</div>";
+  }
+      }
+      else {
+        # code...
+        echo "<div class='alert alert-danger alert-dismissible fade show fixed-top' role='alert'>
+        <strong>Oops!</strong>You have Already Uploaded Please Update If You Want...
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+        <span aria-hidden='true'>&times;</span>
+        </button>
+        </div>";
+      }
+  }
+
+  
+}
+ ?>
