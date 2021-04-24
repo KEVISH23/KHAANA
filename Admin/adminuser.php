b672c1dfec962c7d1084a634aea5164035ffd4d3
akhilshah26 <akhilshah192000@gmail.com>
2021-04-24 12:04:03 +0530

    ok

--- Admin/adminuser.php
+++ Admin/adminuser.php
@@ -5,7 +5,7 @@ include("functions.php");
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Bootstrap 4 Sidebar Menu Responsive with Sub menu  Create Responsive Side Navigation</title>
+    <title>Customer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
