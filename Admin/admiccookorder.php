d3425ecd22ff9fe5602539283c67cd4ccaeba8a7
KEVISH23 <kevishthakkar001@gmail.com>
2021-04-30 16:39:26 +0530

    jai hind

--- Admin/admiccookorder.php
+++ Admin/admiccookorder.php
@@ -108,7 +108,7 @@ if (!isset($_SESSION['adminname'])) {
    <div class="container mt-5">
      <div class="row">
        <div class="col-lg-8 offset-2">
-        <form>
+        <form method="post" action="admiccookorder.php">
           <div class="form-group flabel">
             <label for="inputdelivery">Enter ID</label>
             <input type="text" class="form-control" id="inputdelivery" name="custid"  placeholder="Enter Customer ID" required>
@@ -116,7 +116,7 @@ if (!isset($_SESSION['adminname'])) {
           <div class="container">
             <div class="row">
               <div class="col-md-12 col-lg-12 col-sm-12 text-center viewper">
-                <button type="submit" class="btn btn-success">View Details</button>
+                <button type="submit" name="submit" class="btn btn-success">View Details</button>
               </div>
             </div>
           </div>     
@@ -140,18 +140,79 @@ if (!isset($_SESSION['adminname'])) {
 <?php
 if(isset($_POST['submit'])){
   $cid = $_POST['custid'];
+  #echo $cid;
   $q = "select * from user where user_id = $cid";
   $r = mysqli_query($con,$q);
   if($r){
       $rc = mysqli_num_rows($r);
+      #echo $rc;
       if($rc > 0){
           $q1 = "select * from order_master where user_id = $cid";
           $r1 = mysqli_query($con,$q1);
           if($r1){
           $rc1 = mysqli_num_rows($r1);
+          #echo $rc1;
           if($rc1 > 0){
               while($row1 = mysqli_fetch_array($r1)){
                   $menuid = $row1['menu_id'];
+                  $packid = $row1['package_id'];
+                  if ($menuid==0) {
+                    # code...
+                    $q3 = "select menu_id,package_days,package_price from package where package_id=$packid";
+                    $r3 = mysqli_query($con,$q3);
+                    if ($r3) {
+                      # code...
+                      while ($row3 = mysqli_fetch_array($r3)) {
+                        # code...
+                        $mid = $row3['menu_id'];
+                        $pdays = $row3['package_days'];
+                        $pprice = $row3['package_price'];
+                        $q4 = "select m_image,m_details,m_name from menu where m_id = $menuid";
+                  $r4 = mysqli_query($con,$q4);
+                  if($r4){
+                      while($row4 = mysqli_fetch_array($r4)){
+                          $mimage = $row4['m_image'];
+                          $mdetails = $row4['m_details'];
+                          $mname = $row4['m_name'];
+                          
+                          
+                         /* echo "
+                          <div class='container d-flex justify-content-center'>
+                          <div class='card mt-3' style='width: 18rem;'>
+                            <img class='card-img-top' src='../cook/menuimages/$mimage' alt='Card image cap'>
+                            <div class='card-body'>
+                              <h2>Dish Name:- $mname</h2>
+                               <ul class='list-group list-group-flush'>
+                              <li class='list-group-item'>Details: $mdetails</li>
+                              <li class='list-group-item'>Duration: $pdays </li>
+                              <li class='list-group-item'>$pprice</li>
+                            </ul>
+                            </div>
+                          </div>
+                          </div>";*/
+                          echo "<div class='container d-flex justify-content-center mt-4 mb-4'>
+                          <div class='card' style='width: 18rem;'>
+                          <img src='../cook/menuimages/$mimage' class='card-img-top' alt='Cook Image'>
+                          <div class='card-body'>
+                            <h5 class='card-title'>Name: $mname</h5>
+
+                          </div>
+                          <ul class='list-group list-group-flush'>
+                            <li class='list-group-item'>Details: $mdetails</li>
+                            <li class='list-group-item'>Duration: $pdays</li>
+                            <li class='list-group-item'>Price: $pprice</li>
+                           
+                          </ul>
+                          
+                        </div>
+                        </div>  
+                          ";
+                      }
+                  }
+                      }
+                    }
+                  }
+                  else{
                   $q2 = "select m_image,m_details,m_name,m_price from menu where m_id = $menuid";
                   $r2 = mysqli_query($con,$q2);
                   if($r2){
@@ -162,23 +223,46 @@ if(isset($_POST['submit'])){
                           $mprice = $row2['m_price'];
                           $pro = ($mprice/100)*10;
                           $fp = $mprice + $pro;
-                          echo "<div class='card' style='width: 18rem;'>
-                            <img class='card-img-top' src='../cook/menuimages/$mimage' alt='Card image cap'>
-                            <div class='card-body'>
-                              <h2>Dish Name:- $mname</h2>
-                               <ul class='list-group list-group-flush'>
-                              <li class='list-group-item'>$mdetails</li>
-                              <li class='list-group-item'>$fp</li>
-                            </ul>
-                            </div>
-                          </div>";
+                          echo "<div class='container d-flex justify-content-center mt-4 mb-4'>
+                          <div class='card' style='width: 18rem;'>
+                          <img src='../cook/menuimages/$mimage' class='card-img-top' alt='Cook Image'>
+                          <div class='card-body'>
+                            <h5 class='card-title'>Name: $mname</h5>
+
+                          </div>
+                          <ul class='list-group list-group-flush'>
+                            <li class='list-group-item'>Details: $mdetails</li>
+                            <li class='list-group-item'>Duration: --------</li>
+                            <li class='list-group-item'>Price: $mprice</li>
+                           
+                          </ul>
+                          
+                        </div>
+                        </div>  
+                          ";
                       }
                   }
+                }
               }
           }
+          else{
+            echo "<div class='alert alert-danger alert-dismissible fade show fixed-top' role='alert'>
+               <strong>Oops!</strong>User has not placed any order...
+               <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+               <span aria-hidden='true'>&times;</span>
+               </button>
+               </div>";
+        }
               
           }
       }
+      else{
+        echo "<div class='alert alert-danger alert-dismissible fade show fixed-top' role='alert'>
+           <strong>Oops!</strong>No Such User Found...
+           <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+           <span aria-hidden='true'>&times;</span>
+           </button>
+           </div>";}
   }
 }
 ?>
